fc7e33f6694c677f2bc0fe8a2734d7a25d5ff472
Fetivol <Fetisowova@gmail.com>
2023-07-24 11:08:33 +0200

    scroll + css

--- src/index.js
+++ src/index.js
@@ -7,32 +7,44 @@ const ref = {
   formEl: document.querySelector('.search-form'),
   galerryEl: document.querySelector('.gallery'),
   loadButEl: document.querySelector('.load-more'),
+  scrollUpEl: document.querySelector('.scroll-up'),
 };
 
 let gallery = new SimpleLightbox('.gallery a');
 
 ref.formEl.addEventListener('submit', searchImages);
 ref.loadButEl.addEventListener('click', loadMoreImages);
+ref.scrollUpEl.addEventListener('click', () =>
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+);
 
 ref.loadButEl.style.display = 'none';
 let page = 1;
+
 async function searchImages(event) {
   event.preventDefault();
   page = 1;
   await fetchImages(ref.formEl.elements.searchQuery.value.trim(), page)
     .then(data => {
-      if (data.totalHits === 0) {
+      if (
+        data.totalHits === 0 ||
+        ref.formEl.elements.searchQuery.value === ''
+      ) {
         Notiflix.Notify.failure(
           "Sorry, we didn't find anything! Try changing your search!"
         );
         return;
       }
       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
       const imageCards = data.hits.map(item => createCard(item)).join('');
       ref.galerryEl.innerHTML = imageCards;
+
       gallery.refresh();
+
       if (data.totalHits <= 40) {
         ref.loadButEl.style.display = 'none';
+        lastElement();
         return;
       }
       ref.loadButEl.style.display = 'block';
@@ -75,6 +87,7 @@ async function loadMoreImages() {
       const imageCards = data.hits.map(item => createCard(item)).join('');
       ref.galerryEl.insertAdjacentHTML('beforeend', imageCards);
       gallery.refresh();
+      scroll();
       pageNumbers(data);
     })
     .catch(error => {
@@ -87,9 +100,39 @@ function pageNumbers(data) {
   if (page !== pagesCount) {
     return;
   }
-  ref.loadButEl.style.display = 'none';
 
-  Notiflix.Notify.info(
-    "We're sorry, but you've reached the end of search results."
-  );
+  ref.loadButEl.style.display = 'none';
+  lastElement();
+}
+
+function scroll() {
+  const { height: cardHeight } =
+    ref.galerryEl.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function lastElement() {
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0,
+  };
+
+  const observer = new IntersectionObserver(onIntersect, options);
+  const lastCard = ref.galerryEl.lastElementChild;
+  observer.observe(lastCard);
+}
+
+function onIntersect(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  });
 }
